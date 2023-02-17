34312f6a7f76b5895dfd8cd5b8a8628239737a77
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-02-17 18:30:30 +0530

    commite

---
+++ index.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+    },
+  });
