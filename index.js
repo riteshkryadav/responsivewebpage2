f54d2f021555648a6de796c43a55a095b14568b0
Ritesh Kumar Yadav <ritesh@itobuz.com>
2023-02-21 12:27:51 +0530

    4th  commite

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.5,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      
-    },
-  });
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
 
-  
+  },
+});
+
+let a, b, c;
+document.getElementById("check").addEventListener("click", () => {
+  event.preventDefault();
+  a = document.getElementById("url");
+  b = document.getElementById("mail");
+  c = document.getElementById("output");
+  if (a.value == "" && b.value == "") {
+    c.innerHTML = `<span style="color:red;">*Please enter the info</span>`;
+    disappear();
+  } else if (a.value == "" || b.value == "") {
+    c.innerHTML = `<span style="color:red;">*Please fill both the info</span>`;
+    disappear();
+  }
+  else {
+    c.innerHTML = `<span style="color:green;">*Your entry is recorded</span>`;
+    a.value = "";
+    b.value = "";
+    disappear();
+  }
+});
+// subscribe- section14
+document.getElementById("check2").addEventListener("click", () => {
+  event.preventDefault();
+  a = document.getElementById("name");
+  b = document.getElementById("mail2");
+  c = document.getElementById("output2");
+  if (a.value == "" && b.value == "") {
+    c.innerHTML = `<span style="color:red;">*Please enter the info</span>`;
+    disappear();
+  } else if (a.value == "" || b.value == "") {
+    c.innerHTML = `<span style="color:red;">*Please fill both the info</span>`;
+    disappear();
+  }
+  else {
+    c.innerHTML = `<span style="color:green;">*Your entry is recorded</span>`;
+    a.value = "";
+    b.value = "";
+    disappear();
+  }
+});
+function disappear() {
+  setTimeout(function () {
+    c.innerHTML = "";
+  }, 3000);
+}
+
